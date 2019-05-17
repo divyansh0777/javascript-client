9dc5c813d32f3384cfa1138564e0037611547096
divyansh0777 <divyansh.mishra@successive.tech>
2019-05-17 18:25:51 +0530

    fix: fixed alingnments

---
+++ training/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(, document.getElementById('root'));
+
+
+serviceWorker.unregister();
